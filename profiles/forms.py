032633fefcb9946356d5c0d3bc442168f5fa084c
Eduardo Alvarez <walojose46@gmail.com>
2018-08-24 10:19:33 -0500

    Created: tokenize card and create stripe customer

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.conf import settings
 
 from django.contrib.auth.models import User
 
 from .models import Profile
+
+import stripe
 
 
 class SignupForm(forms.Form):
@@ -14,12 +17,14 @@ class SignupForm(forms.Form):
 
     password = forms.CharField(
         max_length=128,
-        required=True
+        required=True,
+        widget=forms.PasswordInput()
     )
 
     password_confirmation = forms.CharField(
         max_length=128,
-        required=True
+        required=True,
+        widget=forms.PasswordInput()
     )
 
     first_name = forms.CharField(
@@ -46,45 +51,56 @@ class SignupForm(forms.Form):
         required=True
     )
 
+    stripe_token = forms.CharField(
+        max_length=30, 
+        widget=forms.HiddenInput()
+    )
+
     def clean_username(self):
-        username = self.cleaned_data['username']
+        username = self.cleaned_data["username"]
 
         username_exists = User.objects.filter(username=username).exists()
 
         if username_exists:
-            raise forms.ValidationError('Username is alreade taken')
+            raise forms.ValidationError("Username is alreade taken")
 
         return username
 
     def clean(self):
         data = super().clean()
 
-        password = data['password']
-        password_confirmation = data['password_confirmation']
+        password = data["password"]
+        password_confirmation = data["password_confirmation"]
 
         if password != password_confirmation:
             raise forms.ValidationError(
-                'Password and Password confirmation does not match')
+                "Password and Password confirmation does not match")
 
         return data
 
     def save(self):
         data = self.cleaned_data
 
-        data.pop('password_confirmation')
+        data.pop("password_confirmation")
 
         user = User.objects.create_user(
-            username=data['username'],
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            password=data['password'],
-            email=data['email'],
+            username=data["username"],
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            password=data["password"],
+            email=data["email"],
+        )
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe_customer = stripe.Customer.create(
+            source=data["stripe_token"],
+            email=data["email"]
         )
 
         profile = Profile(
             user=user,
-            website=data['website'],
-            phone_number=data['phone_number']
+            website=data["website"],
+            phone_number=data["phone_number"],
+            stripe_user_id=stripe_customer.id
         )
 
         profile.save()
